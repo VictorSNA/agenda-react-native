dec9f8627c4ef16371b96f2a95e26ee81a063d6b
Victor Nakasone <victorsnakasone@gmail.com>
2021-03-21 10:14:45 -0300

    Makes contatos state and methods global for screens

--- App.js
+++ App.js
@@ -1,14 +1,29 @@
 import React, {useState} from 'react';
-import { View } from 'react-native';
-import ContactList from './components/ContactList';
-import GeneralStatusBarColor from './components/GeneralStatusBarColor';
 import LugaresNavigator from './navigation/LugaresNavigator';
-import styles from './styles/AppStyles';
 
 export default function App() {
-  
+  const [contatos, setContatos] = useState(new Map());
+
+  const addContato = (nome, numero) => {
+    if (contatos.get(nome)) {
+      contatos.delete(nome);
+    }
+
+    setContatos(contatos => {
+      console.log(nome + " - " + numero + " salvo com sucesso");
+      return new Map([[nome, numero], ...contatos])
+    })
+  }
+
+  const removeContato = (nome) => {
+    contatos.delete(nome);
+    console.log("Contato "+ nome + " deletado")
+    setContatos(new Map([...contatos]));
+  }
 
   return (
-        <LugaresNavigator/>
+    <LugaresNavigator
+      screenProps={{contatos: contatos, addContato: addContato, removeContato: removeContato}}
+    />
   )
 }
