15eaef7db9ed1ea09b472df5496a4b97b7ad0a4b
Victor Nakasone <victorsnakasone@gmail.com>
2021-03-21 10:14:02 -0300

    Adds missing imports

--- components/BotaoCabecalho.js
+++ components/BotaoCabecalho.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { HeaderButton } from 'react-navigation-header-buttons';
+import { Ionicons } from '@expo/vector-icons';
 import Cores from '../constants/Cores';
 
 const BotaoCabecalho = (props) =>{
